d64a7fdf752acbcf85b736d126e61744dfef3cf4
Kanaknakola <jakub.pecak@o2.pl>
2022-09-14 19:43:50 +0200

    routers

---
+++ src/app/pages/categories/movies-in-category/movies-in-category.component.ts
@@ -0,0 +1,21 @@
+import { Movie } from './../../../models/movie';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { HttpService } from '../../../services/http.service';
+
+@Component({
+  selector: 'app-movies-in-category',
+  templateUrl: './movies-in-category.component.html',
+  styleUrls: ['./movies-in-category.component.css'],
+})
+export class MoviesInCategoryComponent implements OnInit {
+  movies: Observable<Movie[]>;
+
+  constructor(public http: HttpService) {
+    this.movies = this.http.getMoviesFromCategory("");
+  }
+
+  ngOnInit() {}
+}
